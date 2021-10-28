a2d7167b3504f8a9cefbd515653397e6c226da6e
Maxime "Napolitain" Boucher <mxboucher@gmail.com>
2021-10-28 15:59:51 +0200

    fixed cv2 linting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-import cv2
-
+from cv2 import cv2
+import time
 
 class Poursuite:
     def __init__(self, folder):
@@ -9,25 +9,25 @@ class Poursuite:
         :param img_path: path to folder of images
         """
         self.folder = folder
-        self.img_path = os.listdir(folder)  # path, window name
+        self.img_path = os.listdir("sequences/" + folder)  # path, window name
         self.img_path.sort()
         self.index = 0  # index des frames
         self.target_pixels = []
-        self.img = cv2.imread(os.path.join(self.folder, self.img_path[self.index]))  # img left
+        self.img = cv2.imread(os.path.join("sequences/" + self.folder, self.img_path[self.index]))
         self.height, self.width, self.depth = self.img.shape
         self.target_img = 0
-        self.target_height, target_width, target_depth = 0
+        self.target_height, target_width, target_depth = [0, 0, 0]
         self.reload()
-        cv2.setMouseCallback("Poursuite de cible", self.create_target)
+        cv2.setMouseCallback(folder, self.create_target)
 
     def reload(self):
         """
         Met à jour l'image, avec la frame suivante, et met à jour l'index de la prochaine
         :return:
         """
-        self.img = cv2.imread(os.path.join(self.folder, self.img_path[self.index]))  # img left
-        self.find_area()
-        cv2.imshow("Poursuite de cible", self.img)
+        self.img = cv2.imread(os.path.join("sequences/" + self.folder, self.img_path[self.index]))
+        #self.find_area()
+        cv2.imshow(self.folder, self.img)
         self.index = (self.index + 1) % len(self.img_path)
 
     def create_target(self, event, x, y, flags, param):
@@ -49,9 +49,9 @@ class Poursuite:
                         self.target_pixels[0][0]:self.target_pixels[1][0]
                     ]
                     self.target_height, target_width, target_depth = self.target_img.shape
-                    cv2.imshow("Cible", self.target_img)
+                    cv2.imshow(self.folder + ": cible", self.target_img)
                     cv2.rectangle(self.img, self.target_pixels[0], self.target_pixels[1], (0, 0, 255), 2)
-                    cv2.imshow("Poursuite de cible", self.img)
+                    cv2.imshow(self.folder, self.img)
             else:
                 self.target_pixels.clear()
 
@@ -66,7 +66,7 @@ class Poursuite:
         mean_img2 = img2.mean()
         var_img1 = self.target_img.var()
         var_img2 = img2.var()
-        return ((self.target_img - mean_img1).sum() * (img2 - mean_img2).sum()) / (var_img1 * var_img2) ** 0.5
+        return ((self.target_img - mean_img1).sum() * (img2 - mean_img2).sum()) / ((var_img1 * var_img2) ** 0.5)
 
     def find_area(self):
         """
@@ -74,6 +74,11 @@ class Poursuite:
         de corrélation de Pearson.
         :return:
         """
+        self.target_pixels[0][0] = 0
+        self.target_pixels[0][1] = 0
+        self.target_pixels[1][0] = self.width
+        self.target_pixels[1][1] = self.height
+        #while target_pixels[1][0] < self.width and target_pixels[1][1] < self.height:
 
 
 poursuite = Poursuite("SequenceSansVariation")
@@ -81,5 +86,6 @@ while True:
     key = cv2.waitKey(40)  # 25 fps
     if key == 27:  # esc
         cv2.destroyAllWindows()
+        exit(0)
     elif key == 32:  # space
         poursuite.reload()
